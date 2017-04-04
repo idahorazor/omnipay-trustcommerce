c95735d6d3074dea5dfdc43fbc9365714c78fafd
tallchild <tallchild@gmail.com>
2017-04-04 10:09:36 -0600

    Update CaptureRequest.php

--- src/Message/CaptureRequest.php
+++ src/Message/CaptureRequest.php
@@ -8,6 +8,7 @@ class CaptureRequest extends AbstractRequest {
 	public function getData() {
 		$data = $this->getBaseData();
 		$data['action'] = 'postauth';
+		$data['name'] = $this->getCard()->getFirstName() . " " . $this->getCard()->getLastName();
 		$data['amount'] = $this->getAmountInteger();
 		$data['transid'] = $this->getTransactionReference();
 
